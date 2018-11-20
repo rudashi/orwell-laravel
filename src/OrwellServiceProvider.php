f606eddc907ee1b80994fd9630c0364277245d96
rudashi <rudashi@gmail.com>
2018-11-20 11:08:38 +0100

    Fix Defer

--- src/OrwellServiceProvider.php
+++ src/OrwellServiceProvider.php
@@ -7,8 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class OrwellServiceProvider extends ServiceProvider
 {
 
-    protected $defer = true;
-
     public function boot() : void
     {
         $this->loadRoutesFrom(__DIR__.'/api.php');
